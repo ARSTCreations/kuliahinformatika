d89fed6c85f0cacc93b03e69cafb0c8935f786a4
ARSTCreations <anandarizaldyaristyo@gmail.com>
2021-12-04 21:44:23 +0700

    Updated Pemrograman Dasar

---
+++ Pemrograman Dasar/#1/pemrogramanDasar1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){            
+    int bilanganBulat = 12;
+    char huruf = 'a';
+    printf("Halo Gaes!!\n\n");
+    printf("Jangan lupa pake newline ");
+    printf("saat mau print di baris baru\n");
+    printf("biar ga nyambung");
+    printf("kayak gini");
+    printf("\nBisa juga newline sebelum print");
+    printf("\natau kalo mau pake tab kayak \t gini");
+    printf("\nini adalah format desimal --> %d",bilanganBulat);
+    printf("\nini adalah format --> %c",huruf);
+    return 0;
+}
